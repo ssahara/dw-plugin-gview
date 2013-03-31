47d897bc0c7bc62187d594d05e156b736bf007f6
Satoshi Sahara <sahara.satoshi@gmail.com>
2013-03-31 23:39:10 +0900

    fix handling size parameter

--- syntax.php
+++ syntax.php
@@ -74,7 +74,7 @@ class syntax_plugin_gview extends DokuWiki_Syntax_Plugin {
                     $opts['width'] = $matches[1];
                     if (!$matches[2]) $opts['width'].= 'px'; //default to pixel when no unit was set
                     $opts['height'] = $matches[4];
-                    if (!$matches[5]) $opts['width'].= 'px'; //default to pixel when no unit was set
+                    if (!$matches[5]) $opts['height'].= 'px'; //default to pixel when no unit was set
                     continue;
                 } elseif ($matches[2]) {
                     // only height was given
